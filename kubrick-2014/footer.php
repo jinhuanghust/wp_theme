ddddcb67b7b3e0be5864c3e5edab75d1cdb0b3be
Jin Huang <jhuang@niub.la>
2015-02-12 17:28:33 +0800

    Initial check in

---
+++ kubrick-2014/footer.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * The template for displaying the footer.
+ *
+ * Contains the closing of the #content div and all content after
+ *
+ * @package Kubrick 2014
+ */
+?>
+
+	</div><!-- #content -->
+
+	<footer id="colophon" class="site-footer" role="contentinfo">
+		<p class="site-info">
+			<a href="<?php echo esc_url( __( 'http://wordpress.org/', 'kubrick' ) ); ?>"><?php printf( __( 'Proudly powered by %s', 'kubrick' ), 'WordPress' ); ?></a>
+			<span class="sep"> | </span>
+			<?php printf( __( 'Theme: %1$s.', 'kubrick' ), 'Kubrick 2014' ); ?>
+		</p><!-- .site-info -->
+	</footer><!-- #colophon -->
+
+</div><!-- #page -->
+
+<?php wp_footer(); ?>
+
+</body>
+</html>
